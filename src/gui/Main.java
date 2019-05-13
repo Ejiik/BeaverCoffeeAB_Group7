940e21ff77232b261fc3b485090667933099f491
Ejiik <Erik.johansson_93@hotmail.com>
2019-05-13 23:50:38 +0200

    leff

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,9 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,8 +12,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("NewScene.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setTitle("BeaverCoffee AB");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
