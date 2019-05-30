c8991f785736fac5462ee6a18704a195f37752ff
Juhu94 <julian.hultgren@hotmail.com>
2019-05-30 14:13:11 +0200

    Updated GUI
    Updated gui.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -15,7 +15,7 @@ public class Controller {
 		
 	}
 	
-	public void ShowAddProduct(ActionEvent event) throws IOException {
+	public void ShowNewScene(ActionEvent event) throws IOException {
 		String source = ((Button)event.getSource()).getText();
 		System.out.println(source + " Window");
 		
@@ -28,35 +28,59 @@ public class Controller {
 			root = FXMLLoader.load(getClass().getClassLoader().getResource("gui/AddCustomerWindow.fxml"));
 		}
 		else if(source.equals("Add Employee")) {
-			
+			root = FXMLLoader.load(getClass().getClassLoader().getResource("gui/AddEmployeeWindow.fxml"));
 		}
 		else if(source.equals("Add Order")) {
 			root = FXMLLoader.load(getClass().getClassLoader().getResource("gui/AddOrderWindow.fxml"));
 		}
-		
+		else if(source.equals("Add Comment")) {
+			root = FXMLLoader.load(getClass().getClassLoader().getResource("gui/AddCommentWindow.fxml"));
+		}
+		else if(source.equals("Add Employer")) {
+			root = FXMLLoader.load(getClass().getClassLoader().getResource("gui/AddEmployerWindow.fxml"));
+		}
+	
 		Scene scene = new Scene(root);
 		Stage stage = new Stage();
-		stage.setTitle("Add Product");
+		stage.setTitle(source);
 		stage.setScene(scene);
 		stage.show();	
 	}
 	
-	/*
+	/**
 	 * Method for adding a product to the database
 	 */
 	public void AddProduct() {
 		System.out.println("Added Product");
 	}
-	/*
+	/**
 	 * Method for adding a customer to the database
 	 */
 	public void AddCustomer() {
 		System.out.println("Added Customer");
 	}
-	/*
+	/**
 	 * Method for adding a order to the database
 	 */
 	public void AddOrder() {
 		System.out.println("Added Customer");
+	}
+	/**
+	 * Method for adding a employee to the database
+	 */
+	public void AddEmployee() {
+		System.out.println("Added Employee");
+	}
+	/**
+	 * Method for adding a employer to the database
+	 */
+	public void AddEmployer() {
+		System.out.println("Added Employer");
+	}
+	/**
+	 * Method for adding a comment to a employee
+	 */
+	public void AddComment() {
+		System.out.println("Added Comment");
 	}
 }
